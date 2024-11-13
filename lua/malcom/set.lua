883cebd599fdeeca8ca6d6b96938fbf1d9c9c0d3
Malcom Alister Gardiner <malcom.alg@gmail.com>
2024-11-13 15:12:15 +0100

    fixed tab and listchars

--- lua/malcom/set.lua
+++ lua/malcom/set.lua
@@ -8,7 +8,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
@@ -24,3 +24,6 @@ vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append('@-@')
 
 vim.opt.updatetime = 50
+
+vim.opt.listchars = { eol = '', tab = '-->' }
+vim.opt.list = true
