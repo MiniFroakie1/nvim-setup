1249f7d2f359bd0566500e2baf4e328488da0c4d
malcom <malcom.alg@gmail.com>
2025-02-11 21:40:41 +0100

    removed the transparent effects from the colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme rose-pine");
-	end
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+        require("rose-pine").setup({ style = { transparency = false } })
+
+        vim.cmd("colorscheme rose-pine");
+    end
 }
