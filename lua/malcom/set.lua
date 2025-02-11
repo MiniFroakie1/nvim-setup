4cf2ce9713a2dab8d890d2c59b7168294522a37e
malcom <malcom.alg@gmail.com>
2025-02-11 21:41:20 +0100

    changed how tab works and added a column at 80 characters

--- lua/malcom/set.lua
+++ lua/malcom/set.lua
@@ -8,7 +8,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
@@ -27,3 +27,5 @@ vim.opt.updatetime = 50
 
 vim.opt.listchars = { eol = '', tab = '-->' }
 vim.opt.list = true
+
+vim.opt.colorcolumn = "80"
